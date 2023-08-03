f0ef7de2cf5f5165c70ee60ed36feab8ec9ba688
munir79 <jakirhossainmunir79@gmail.com>
2023-08-03 11:12:49 +0600

    signout

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import logo from './logo.svg';
 import './App.css';
 
 // step 1 : import getAuth and app;
-import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
+import {GoogleAuthProvider, getAuth, signInWithPopup, signOut} from 'firebase/auth'
 import app from './Firebase/Firebase.init';
+import { useState } from 'react';
 const auth=getAuth(app);
 function App() {
 
   // step:2 provider 
 
   const provider =new GoogleAuthProvider()
+   const {user,setUSer}=useState({})
 
+  // step :3 event handelar 
   const googleHandleSignIn=()=>{
     signInWithPopup(auth,provider)
     .then(result=>{
       const user=result.user;
+      setUSer(user);
       console.log(user);
 
 
@@ -23,9 +27,23 @@ function App() {
       console.log('error:',error);
     })
   }
+
+  const handleSignOut=()=>{
+    signOut(auth)
+    .then(()=>{
+      setUSer({});
+    })
+    .catch(()=>{
+      setUSer({});
+    })
+  }
   return (
     <div className="App">
      <button onClick={googleHandleSignIn}> Sign in</button>
+     <button onClick={handleSignOut} > signOut</button>
+     <div>
+       <h3> userName: {user.email} </h3>
+     </div>
     </div>
   );
 }
