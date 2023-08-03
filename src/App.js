8af3bf95d9515824baffacd20b881077f2e120d0
munir79 <jakirhossainmunir79@gmail.com>
2023-08-03 10:49:31 +0600

    SignIn with Google PopUp

--- src/App.js
+++ src/App.js
@@ -1,23 +1,31 @@
 import logo from './logo.svg';
 import './App.css';
 
+// step 1 : import getAuth and app;
+import {GoogleAuthProvider, getAuth, signInWithPopup} from 'firebase/auth'
+import app from './Firebase/Firebase.init';
+const auth=getAuth(app);
 function App() {
+
+  // step:2 provider 
+
+  const provider =new GoogleAuthProvider()
+
+  const googleHandleSignIn=()=>{
+    signInWithPopup(auth,provider)
+    .then(result=>{
+      const user=result.user;
+      console.log(user);
+
+
+    })
+    .catch(error=>{
+      console.log('error:',error);
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <button onClick={googleHandleSignIn}> Sign in</button>
     </div>
   );
 }
---
+++ src/Firebase/Firebase.init.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDYKg33Re_p-j_opJiGZqurfZ7RUQodBX4",
+  authDomain: "munir35-b8b80.firebaseapp.com",
+  projectId: "munir35-b8b80",
+  storageBucket: "munir35-b8b80.appspot.com",
+  messagingSenderId: "729636025908",
+  appId: "1:729636025908:web:f478db1a7e0bf5bdfafc61"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
